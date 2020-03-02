63ab3b2d1291a07bbf8d35b3bfb9340d05815b1c
HYEZ <oohyejioo@gmail.com>
2020-03-03 00:26:25 +0900

    [#7]탑 스택 수정

--- 프로그래머스/[#7]탑.py
+++ 프로그래머스/[#7]탑.py
@@ -4,27 +4,53 @@ def solution (heights):
 	# heights : stack
 	stk = heights
 	tmp = []
-	for i in range(len(stk)):
-		if len(tmp) > 0: # tmp가 빈 값이 아니면
+	is_cur = False
+	cur = 0
+
+	while len(stk) >= 1:
+
+		if is_cur == True and len(tmp) > 0: # tmp가 빈 값이 아니면
 			# 꺼내서 스택에 넣음
-			for i in range(len(tmp)):
-				stk.append(tmp.pop())
+			while True:
+				s = stk.pop()
+				if(s > answer[-1]):
+					break
+				else:
+					tmp.append(s)
+			print("t", tmp)
+			while len(tmp) > 0:
+				t = tmp.pop()
+				stk.append(t)
+			print('s', stk, answer)
+
+		# print(stk)
 
 		cur = stk.pop()
-		
 
-		print(heights.pop())
+		# cur = stk.pop()
+		# print(cur, stk[-1])
+
+		if(cur >= stk[-1]): # 밑에 스택보다 더 크면 => 다른 스택에다가 임시로 넣어둠
+			is_cur = True
+			print("cur", cur)
+			tmp.append(stk.pop())
+
+			
+		else: # 다음께 더 크면 답에다 넣고 스택에서 아예 빼버림
+			is_cur = False
 
 
-	
+		answer.append(cur)
 
+
+	answer.reverse()
 	return answer
 
 
 
 
-heights = [6,9,5,7,4]
+# heights = [6,9,5,7,4]
 heights = [3,9,9,3,5,7,2]
-heights = [1,5,3,6,7,6,5]
-print(solution(heights))
+# heights = [1,5,3,6,7,6,5]
+print("A", solution(heights))
 
