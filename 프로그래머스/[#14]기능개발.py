9544ac941e97a4fe74c7092d10bb1ffb4e334a64
HYEZ <oohyejioo@gmail.com>
2020-05-26 14:46:55 +0900

    [#15]프린터

--- 프로그래머스/[#14]기능개발.py
+++ 프로그래머스/[#14]기능개발.py
@@ -16,6 +16,18 @@ def solution(progresses, speeds):
 
 	return answer
 
+def solution2(progresses, speeds):
+	prev = 0
+	answer = []
+	for p, k in zip(progresses, speeds):
+		a = math.ceil((100-p)/k)
+		if prev >= a:
+			answer[-1] += 1
+		else:
+			prev = a
+			answer.append(1)
+	return answer
+
 progresses =  [40, 93, 30, 55, 60, 65]
 speeds = [60, 1, 30, 5 , 10, 7]
-print(solution(progresses, speeds))
+print(solution2(progresses, speeds))
