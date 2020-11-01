ad9a819ec4c6bcb69426b649ae1ec8f5ef9efba2
HYEZ <oohyejioo@gmail.com>
2020-11-01 17:47:52 +0900

    1으로만들기

--- 2020/DP/fibonacci.py
+++ 2020/DP/fibonacci.py
@@ -3,6 +3,7 @@
 d = [0] * 100
 
 def fibo(a):
+    print('f(',a,')', end=' ')
     if a == 1 or a == 2:
         return 1
 
@@ -12,16 +13,16 @@ def fibo(a):
     d[a] = fibo(a-1) + fibo(a-2)
     return d[a]
 
-print(fibo(99))
-
+print(fibo(6))
 # 반복 (보텀업방식-상향식)
 d = [0] * 100
 
 d[1] = 1
 d[2] = 1
-n = 99
+n = 6
 
 for i in range(3, n+1):
+    print('f(',i,')', end=' ')
     d[i] = d[i-1] + d[i-2]
 
 print(d[n])
