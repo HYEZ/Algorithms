4e31a8f5d817bb911a2a8efe066929057530040c
HYEZ <oohyejioo@gmail.com>
2020-11-09 17:52:56 +0900

    입국심사

---
+++ 2020/Stack_and_Queue/기능개발.py
@@ -0,0 +1,10 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42586
+
+def solution(progresses, speeds):
+    
+
+
+progresses = [93, 30, 55] # 진도
+speeds = [1, 30, 5]	# 작업속도
+
+print(solution(progresses, speeds))
