fd28f0ba023d1dd47e1c0ac35a6d2f5024a50572
HYEZ <oohyejioo@gmail.com>
2020-11-12 14:10:47 +0900

    다리를지나는 트럭 - 주석

--- 2020/Stack_and_Queue/다리를지나는트럭.py
+++ 2020/Stack_and_Queue/다리를지나는트럭.py
@@ -3,23 +3,23 @@
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    total_weight = truck_weights[0]
-    truck_weights = deque(truck_weights)
-    q = deque([truck_weights.popleft()])
+    total_weight = truck_weights[0] # 다리 안에 있는 트럭들의 총 무게
+    truck_weights = deque(truck_weights) # 다리를 아직 지나지 않은 트럭들
+    q = deque([truck_weights.popleft()]) # 다리에 있는 트럭들
     t = 0
-    while q:
+    while q: # 다리에 있는 트럭들이 있으면
         t += 1
-        if len(q) == bridge_length:
+        if len(q) == bridge_length: # 다리에 있는 트럭들의 개수가 꽉 차면
             total_weight -= q.popleft()
 
-        if not truck_weights:
+        if not truck_weights: # 다리를 아직 지나지 않은 트럭이 없으면 break
             break
 
-        if total_weight + truck_weights[0] <= weight:
+        if total_weight + truck_weights[0] <= weight: # 새로운 트럭의 무게가 안넘어가면
             p = truck_weights.popleft()
             total_weight += p
             q.append(p)
-        else:
+        else: # 무게 넘어가면 일단 0으로 채우고 다시 루프 돌기
             q.append(0)
 
     return t + bridge_length
