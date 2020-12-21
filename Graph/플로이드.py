7c1564f38fd2744d38f8567558b136ececcda10d
HYEZ <oohyejioo@gmail.com>
2020-12-21 18:58:34 +0900

    플로이드

--- Graph/플로이드.py
+++ Graph/플로이드.py
@@ -3,7 +3,8 @@
 n = int(input())
 m = int(input())
 
-graph = [[1000000] * (n+1) for _ in range(n+1)]
+INF = 1e9
+graph = [[INF] * (n+1) for _ in range(n+1)]
 
 for _ in range(m):
     a, b, c = map(int, input().split())
@@ -23,6 +24,8 @@ for k in range(1, n+1):
 
 for i in range(1, n+1):
     for j in range(1, n+1):
-        print(graph[i][j], end =' ')
-    
+        if graph[i][j] == INF:
+            print(0, end=' ')
+        else:
+            print(graph[i][j], end =' ')
     print()
