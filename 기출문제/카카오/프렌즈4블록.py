a7ad5e647eb3e0b6c5f94d2e1933bde7d810a9b8
HYEZ <oohyejioo@gmail.com>
2021-01-14 14:51:09 +0900

    프렌즈4블록

--- 기출문제/카카오/프렌즈4블록.py
+++ 기출문제/카카오/프렌즈4블록.py
@@ -1,47 +1,96 @@
+# https://programmers.co.kr/learn/courses/30/lessons/17679
+# 카카오 2018 신입 공채 1차
+# 풀이) 구현, 완전탐색, 스택
+# 참고) BFS로 풀었더니 시간초과남 (맨 아래 코드)
+
 from collections import deque
 import copy
 
 def solution(n, m, board):
-    
     board = list(map(list, board))
+    answer = 0
     while True:
-        visited = [[0] * m for _ in range(n)]
+        visited = [[0] * m for _ in range(n)]   
 
-        for i in range(n):
-            for j in range(m):
-                if board[i][j] == 0:
-                    visited[i][j] = 1
+        visited, cnt = chk_visited(n, m, board, visited)
+        if cnt == 0:
+            break
 
-        # print('board', board)
-        for i in range(n-1):
-            for j in range(m-1):
-                # v = copy.deepcopy(visited)
-                # board = copy.deepcopy(board)
-                visited = bfs(board, i, j, visited)            
-        # print('visited', visited)
-        new_board = make_block(board, visited)
-        # print('new_board', new_board)
-        cnt1, cnt2 = 0, 0
-        for x in range(n):
-            cnt1 += board[x].count(0)
-            cnt2 += new_board[x].count(0)
-        # print(cnt1, cnt2)
-        
-        if cnt1 == cnt2:
-            # for k in range(n):
-            #     print(board[k])
-            # print(board.count(0))
-            return cnt1
+        board = make_block(board, visited)
 
-        # print(visited)
-        board = new_board
+    for x in range(n):
+        answer += board[x].count(0)
+
+    return answer
+
+def chk_visited(n, m, board, visitied):
+    res = []
+    for i in range(n-1): 
+        for j in range(m-1):
+            tmp = board[i][j]
+            if tmp == 0:
+                continue
+            if board[i+1][j] == tmp and board[i][j+1] == tmp and board[i+1][j+1] == tmp:
+                visitied[i][j] = 1
+                visitied[i+1][j] = 1
+                visitied[i][j+1] = 1
+                visitied[i+1][j+1] = 1
+                res += [board[i+1][j], board[i][j+1], board[i+1][j+1]]
+    
+    return visitied, len(res)
 
 
-    # for i in range(n):
-    #     print(board[i])
 
-    # for i in range(n):
-    #     print(visited[i])
+def make_block(board, visited):    
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if visited[i][j] == 1:
+                board[i][j] = 0
+
+    
+    for i in range(len(board[0])):
+        value = []
+        for j in range(len(board)):
+            if board[j][i] != 0:
+                value.append(board[j][i])
+
+        for j in range(len(board) - 1, -1, -1):
+            if len(value) > 0:
+                v = value.pop()
+                board[j][i] = v
+            else:
+                board[j][i] = 0
+
+    return board
+
+    
+
+
+m = 4
+n = 5
+board = ['CCBDE', 'AAADE', 'AAABF', 'CCBBF']	
+
+# m = 6
+# n = 6
+# board = ['TTTANT', 'RRFACC', 'RRRFCC', 'TRRRAA', 'TTMMMF', 'TMMTTJ']
+n = 6
+m = 5
+board = ['AAAAAA', 'BBAATB', 'BBAATB', 'JJJTAA', 'JJJTAA']
+
+# # 답 32
+# n = 6
+# m = 6
+# board = ['AABBEE','AAAEEE','VAAEEV','AABBEE','AACCEE','VVCCEE' ]
+
+n = 4
+m = 4
+
+board = ['ABCD', 'BACE', 'BCDD', 'BCDD']
+
+
+
+print(solution(m, n, board))            
+
 
 def bfs(board, x, y, visitied):
     
@@ -49,7 +98,7 @@ def bfs(board, x, y, visitied):
     dy = [0, 1, 1]
     q = deque([(x, y)])
     
-    while q:
+    while q: #100
         x, y = q.popleft()
 
         chk = 3
@@ -73,48 +122,3 @@ def bfs(board, x, y, visitied):
                 q.append((nx, ny))
     
     return visitied
-
-def make_block(board, visited):
-    board = copy.deepcopy(board)
-    for i in range(len(board[0])):
-        idx = 0
-        value = []
-        for j in range(len(board)):
-            
-            if board[j][i] != 0:
-                value.append(board[j][i]) 
-            # print(visited)
-            if visited[j][i] == 1 and board[j][i] != 0:
-                value.pop()
-                idx = j
-            
-            if idx != 0 and visited[j][i] == 0:
-                break
-            # print(visited[j][i], end=' ')
-        
-        # print(idx, value)
-        if idx + 1 >= len(board):
-            idx -= 1
-
-        if idx != 0: #  and idx+1 < len(board)
-            for j in range(idx+1, -1, -1):
-                if len(value) == 0:
-                    board[j][i] = 0   
-                else: 
-                    v = value.pop()
-                    board[j][i] = v  
-            
-    
-    return board
-
-
-
-
-m = 4
-n = 5
-board = ['CCBDE', 'AAADE', 'AAABF', 'CCBBF']	
-
-# m = 6
-# n = 6
-# board = ['TTTANT', 'RRFACC', 'RRRFCC', 'TRRRAA', 'TTMMMF', 'TMMTTJ']
-print(solution(m, n, board))            
