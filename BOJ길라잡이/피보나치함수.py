c1d2fd60770b4a0b647e050578fecb72bbeb6206
HYEZ <oohyejioo@gmail.com>
2021-02-01 17:36:03 +0900

    피보나치 함수

--- BOJ길라잡이/피보나치함수.py
+++ BOJ길라잡이/피보나치함수.py
@@ -1,24 +1,23 @@
 # https://www.acmicpc.net/problem/1003
-# 풀이) 전역변수 
-import sys
-sys.setrecursionlimit(10**7)
-
-c0, c1 = 0, 0
-def fibonacci(n):
-    global c0, c1
-    if n == 0:
-        c0 += 1
-        return 0
-    elif n == 1:
-        c1 += 1
-        return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
+# 풀이) DP
 
 t = int(input())
 for _ in range(t):
     n = int(input())
-    c0, c1 = 0, 0
-    fibonacci(n)
-    print(c0, c1)
+    if n == 0:
+        print('1 0')
+        continue
+    elif n == 1:
+        print('0 1')
+        continue
+    
+    dp0, dp1 = [0] * (n+1), [0] * (n+1)
+    dp0[0], dp0[1] = 1, 0
+    dp1[0], dp1[1] = 0, 1
+
+    for i in range(2, n+1):
+        dp0[i] = dp0[i-1] + dp0[i-2]
+        dp1[i] = dp1[i-1] + dp1[i-2]
+        
+    print(dp0[-1], dp1[-1])
 
