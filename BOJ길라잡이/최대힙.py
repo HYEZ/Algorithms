802dac600ece560c52ea9713d8b5d6b460e12a03
HYEZ <oohyejioo@gmail.com>
2021-02-09 11:45:01 +0900

    최대힙

---
+++ BOJ길라잡이/최대힙.py
@@ -0,0 +1,23 @@
+# https://www.acmicpc.net/problem/11279
+# 풀이) heapq
+
+import heapq
+
+n = int(input())
+q = []
+res = []
+
+for _ in range(n):
+    o = int(input())
+    if o == 0:
+        if len(q) == 0:
+            res.append(0)
+        else:
+            res.append(-heapq.heappop(q))
+    else:
+        heapq.heappush(q, -o)
+
+for i in res:
+    print(i)
+
+        
