9b6650a56ea3a5cea93adf998509634c7e721efb
HYEZ <oohyejioo@gmail.com>
2021-02-11 17:08:19 +0900

    2xn타일링

---
+++ BOJ길라잡이/2xn타일링.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/11726
+# 풀이) DP
+
+n = int(input())
+
+dp = [0] * (n+1)
+
+
+dp[1] = 1
+
+if n >= 2:
+    dp[2] = 2
+
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[-1]%10007)
