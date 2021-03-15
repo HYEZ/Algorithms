254767701abef90801dcf77af4e6ba0d219beba1
HYEZ <oohyejioo@gmail.com>
2021-03-15 17:10:56 +0900

    메뉴리뉴얼:

--- 기출문제/카카오/메뉴리뉴얼.py
+++ 기출문제/카카오/메뉴리뉴얼.py
@@ -1,9 +1,6 @@
 # https://programmers.co.kr/learn/courses/30/lessons/72411
 # 풀이) 완전탐색, 조합, 해쉬
 
-# 이전에 각 손님들이 주문할 때 가장 많이 함께 주문한 단품메뉴들을 코스요리 메뉴로 구성하기로 했습니다.
-# 단, 코스요리 메뉴는 최소 2가지 이상의 단품메뉴로 구성하려고 합니다. 
-# 또한, 최소 2명 이상의 손님으로부터 주문된 단품메뉴 조합에 대해서만 코스요리 메뉴 후보에 포함하기로 했습니다.
 from itertools import combinations
 from collections import defaultdict
 
@@ -12,9 +9,13 @@ def solution(orders, course):
     orders = list(map(lambda a: sorted(list(a)), orders))
     res = []
     for c in course:
-        cases = list(map(lambda a: list(a), combinations(types, c)))
-        print(cases)
-        d = defaultdict(int)        
+        cases = []
+        for order in orders:
+            cases += list(map(lambda a: list(a), combinations(order, c)))
+        
+        cases = list(set(tuple(map(tuple, cases))))
+
+        d = defaultdict(int)
         for case in cases:
             for order in orders:
                 if set(case) == set(case).intersection(set(order)):
