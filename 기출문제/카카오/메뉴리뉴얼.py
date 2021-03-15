53fcf1c864820a2d30bbaead686a2a2af01c1bdb
HYEZ <oohyejioo@gmail.com>
2021-03-15 16:39:16 +0900

    메뉴리뉴얼

--- 기출문제/카카오/메뉴리뉴얼.py
+++ 기출문제/카카오/메뉴리뉴얼.py
@@ -1,5 +1,5 @@
 # https://programmers.co.kr/learn/courses/30/lessons/72411
-# 풀이) 완전탐색, 조합
+# 풀이) 완전탐색, 조합, 해쉬
 
 # 이전에 각 손님들이 주문할 때 가장 많이 함께 주문한 단품메뉴들을 코스요리 메뉴로 구성하기로 했습니다.
 # 단, 코스요리 메뉴는 최소 2가지 이상의 단품메뉴로 구성하려고 합니다. 
@@ -10,24 +10,25 @@ from collections import defaultdict
 def solution(orders, course):
     types = sorted(list(set(list(''.join(orders)))))
     orders = list(map(lambda a: sorted(list(a)), orders))
-    
+    res = []
     for c in course:
         cases = list(map(lambda a: list(a), combinations(types, c)))
-        d = defaultdict()
-        
+        print(cases)
+        d = defaultdict(int)        
         for case in cases:
             for order in orders:
-                # print(case, order, set(case) in set(order))
-                if set(case) in set(order):
-                    print(case, order)
-                    d[''.join(case)] += 1
-        print(d)
-                
-        
-        
-
-
-    print(orders)
+                if set(case) == set(case).intersection(set(order)):
+                    key = ''.join(case)
+                    d[key] += 1
+        if len(d.values()) == 0:
+            continue
+        max_value = max(d.values())
+        if max_value < 2:
+            continue              
+        for key, value in d.items():
+            if max_value == value:
+                res.append(key)
+    return sorted(res)
 
 
 orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]	
