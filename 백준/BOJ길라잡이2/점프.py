b0f73e76f51148d508a8a4b5640508b996faf675
HYEZ <oohyejioo@gmail.com>
2021-04-09 00:49:13 +0900

    두부장수 장홍준

--- 백준/BOJ길라잡이2/점프.py
+++ 백준/BOJ길라잡이2/점프.py
@@ -11,13 +11,13 @@ dp[0][0] = 1
 
 for i in range(n):
     for j in range(n):
-        if i == n-1 and j == n-1:
+        if i == n-1 and j == n-1: 
             break
         
         jump = arr[i][j]
         if i+jump < n:
             dp[i+jump][j] += dp[i][j]
-            
+
         if j+jump < n:
             dp[i][j+jump] += dp[i][j]
   
