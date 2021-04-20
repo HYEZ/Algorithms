712c0459bd7109edad450d06f6326e23f975d883
HYEZ <oohyejioo@gmail.com>
2021-04-20 18:11:59 +0900

    최대값과 최소값

---
+++ 프로그래머스/최댓값과최솟값.py
@@ -0,0 +1,10 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12939
+# 풀이) 
+
+def solution(s):
+    s = list(map(int, s.split()))
+    res = ''.join(str(min(s)) + ' ' + str(max(s)))
+    return res
+
+s = "-1 -2 -3 -4"
+print(solution(s))
