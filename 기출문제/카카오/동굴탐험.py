95334653e15835858f6d282058d060b8cd7a44b7
HYEZ <oohyejioo@gmail.com>
2021-04-28 18:52:27 +0900

    동굴탐험

--- 기출문제/카카오/동굴탐험.py
+++ 기출문제/카카오/동굴탐험.py
@@ -8,14 +8,22 @@ sys.setrecursionlimit(10**7)
 def dfs(now):
     global graph, priority, post_order, visited
 
-    if not visited[now] and (pre_order[now] == 0 or visited[pre_order[now]]): 
-        # print(now)
-        visited[now] = True
-        for i in graph[now]:
-            dfs(i)
+    if visited[now]: # 이미 방문했으면 return
+        return False
+    
+    if pre_order[now] != 0 and not visited[pre_order[now]]: # 이전 노드 방문 안했을 때 
+        post_order[pre_order[now]] = now
+        return False
 
-        if post_order[now] != 0:
-            dfs(post_order[now])
+    
+    visited[now] = True # 방문처리
+
+    for i in graph[now]:
+        dfs(i)
+
+    if post_order[now] != 0:
+        dfs(post_order[now])
+    
 
 
 def solution(n, path, order):
@@ -25,7 +33,6 @@ def solution(n, path, order):
     post_order = [0] * n 
     for a, b in order:
         pre_order[b] = a
-        post_order[a] = b
 
     visited = [False] * n
     graph = [[] for _ in range(n)]
@@ -36,17 +43,12 @@ def solution(n, path, order):
     if pre_order[0] != 0: #  입구에 선방문이 존재하는 경우
         return False
     
-    visited[0] = True
     for i in graph[0]:
         dfs(i)
     
-    # print(visited)
-
     return n == sum(visited)
 
 
 
 n, path, order = 9,	[[0,1],[0,3],[0,7],[8,1],[3,6],[1,2],[4,7],[7,5]],	[[8,5],[6,7],[4,1]]
-n, path, order = 9, [[0, 1], [0, 3], [0, 7], [8, 1], [3, 6], [1, 2], [4, 7], [7, 5]], [[4, 1], [8, 7], [6, 5]]
-n, path, order = 9, [[8,1],[0,1],[1,2],[0,7],[4,7],[0,3],[7,5],[3,6]]	, [[4,1],[5,2]]	
 print(solution(n, path, order))
