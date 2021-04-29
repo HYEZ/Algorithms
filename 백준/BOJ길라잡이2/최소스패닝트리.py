ec44bd05a9e4c05ea85b40f36daf99ec26c7b29a
HYEZ <oohyejioo@gmail.com>
2021-04-29 15:56:22 +0900

    최소스패닝트리

---
+++ 백준/BOJ길라잡이2/최소스패닝트리.py
@@ -0,0 +1,35 @@
+# https://www.acmicpc.net/problem/1197
+# 풀이) 크루스칼 알고리즘
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+v, e = map(int, input().split())
+edges = []
+parent = [i for i in range(v+1)]
+
+for i in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+
+result = 0
+for cost, a, b in edges:
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
