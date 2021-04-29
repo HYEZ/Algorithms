a95002a5306f8495442b57e690d888ab801d9bb8
HYEZ <oohyejioo@gmail.com>
2021-04-29 16:16:56 +0900

    거스름돈

--- 프로그래머스/거스름돈.py
+++ 프로그래머스/거스름돈.py
@@ -2,11 +2,14 @@
 # 풀이) DP
 
 def solution(n, money):
-    dp = [0] * (n+1)
+    dp = [1] + [0] * n
     
-    
+    for coin in money:
+        for price in range(coin, n+1):
+            dp[price] += dp[price-coin]
 
     return dp[n] % 1000000007
+    
 
 n, money = 5, [1,2,5]	
 print(solution(n, money))
