6eaeb419bc1665aab2ae26434222c82b2e4e297f
HYEZ <oohyejioo@gmail.com>
2021-04-30 15:49:25 +0900

    멀리뛰기

---
+++ 프로그래머스/멀리뛰기.py
@@ -0,0 +1,15 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12914
+# 풀이) DP
+
+def solution(n):
+    if n == 1:
+        return 1
+    
+    dp = [0] * (n+1)
+    dp[1] = 1
+    dp[2] = 2
+    
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] +dp[i-2]
+
+    return dp[n] % 1234567
